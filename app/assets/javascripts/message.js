bc55fe39bae9da83c31a0b408a85a35308b416e6
shun255454 <donp3461@gmail.com>
2020-04-05 17:09:36 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -94,5 +94,7 @@ $(function(){
       alert('error');
     });
   }; 
-  setInterval(reloadMessages, 7000);
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
  });
