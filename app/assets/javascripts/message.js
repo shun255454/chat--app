04cf85f5de71c3c5dd236b991890e433267b96e1
shun255454 <donp3461@gmail.com>
2020-04-06 10:47:29 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -81,6 +81,7 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
+      if (messages.length !== 0) {
       //追加するHTMLの入れ物を作る
       var insertHTML = '';
       //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
@@ -89,6 +90,8 @@ $(function(){
       });
       //メッセージが入ったHTMLに、入れ物ごと追加
       $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
     })
     .fail(function() {
       alert('error');
